b359494e32dc9346e5b1644c106c346e1a35b065
hernok <hernok@hotmail.com>
2022-01-31 12:44:42 +0100

    js update, html card

--- class.js
+++ class.js
@@ -4,17 +4,40 @@ class Animal {
     this.name = name;
     this.country = country;
     this.color = color;
-    
+    this.imgUrl = imgUrl;
   }
 }
+let animalOne = new Animal(
+  "Cat",
+  "Pusur",
+  "Norway",
+  "Gray",
+  "https://petkeen.com/wp-content/uploads/2021/05/grey-cat.jpeg"
+);
+let animalTwo = new Animal(
+  "Ox",
+  "Klara",
+  "Norway",
+  "Black and white",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Cow_female_black_white.jpg/1920px-Cow_female_black_white.jpg"
+);
+console.log(animalOne.name);
+console.log(animalTwo.color);
+console.log(animalOne.species);
+console.log(animalTwo.imgUrl);
+console.log(animalOne.country);
 
-Object
-"species": "Feline",
-"name": "Pusur",
-"country": "Norway",
-"color": "Gray",
-"imgUrl": "https://petkeen.com/wp-content/uploads/2021/05/grey-cat.jpeg"
-"feedAnimal": "",
+var img = new Image();
+var div = document.getElementById("animalOne");
 
-let animalOne = new Animal("Cat", "Pusur", "Norway", "Gray");
-let animalTwo = new Animal("Ox", "Klara", "Norway", "Black and white");
+const animalSpecies = document.getElementsByClassName("animalOneSpecies");
+const animalColor = document.getElementsByClassName("animalOneColor");
+const animalName = document.getElementsByClassName("animalOneName");
+const animalCountry = document.getElementsByClassName("animalOneCountry");
+const AnimalImage = document.getElementsByClassName("");
+
+const animalSpecies = document.getElementsByClassName("animalTwoSpecies");
+const animalColor = document.getElementsByClassName("animalTwoColor");
+const animalName = document.getElementsByClassName("animalTwoName");
+const animalCountry = document.getElementsByClassName("animalTwoCountry");
+const AnimalImage = document.getElementsByClassName("");

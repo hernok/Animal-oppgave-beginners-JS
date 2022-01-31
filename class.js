ccfb337cc5c54dd6a21e6cae4fbc9fae023603e5
hernok <hernok@hotmail.com>
2022-01-31 11:16:44 +0100

    let, class, object update 1

--- class.js
+++ class.js
@@ -4,5 +4,17 @@ class Animal {
     this.name = name;
     this.country = country;
     this.color = color;
+    
   }
 }
+
+Object
+"species": "Feline",
+"name": "Pusur",
+"country": "Norway",
+"color": "Gray",
+"imgUrl": "https://petkeen.com/wp-content/uploads/2021/05/grey-cat.jpeg"
+"feedAnimal": "",
+
+let animalOne = new Animal("Cat", "Pusur", "Norway", "Gray");
+let animalTwo = new Animal("Ox", "Klara", "Norway", "Black and white");
